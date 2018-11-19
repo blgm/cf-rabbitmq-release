1011452931f40c4ef5e8f968e94657d48b0e2569
Ben Tillett <btillett@pivotal.io>
2018-11-19 16:59:32 +0000

    WIP
    [#161337142]
    
    Co-authored-by: Andrea Nodari <anodari@pivotal.io>

--- jobs/rabbitmq-server/templates/post-deploy.bash
+++ jobs/rabbitmq-server/templates/post-deploy.bash
@@ -2,8 +2,7 @@
 
 [ -z "$DEBUG" ] || set -x
 
-PATH=/var/vcap/jobs/rabbitmq-server/bin:$PATH
-
 node-check "post-deploy"
 cluster-check "post-deploy"
-add-rabbitmqctl-to-path
+
+. /var/vcap/jobs/rabbitmq-server/env
